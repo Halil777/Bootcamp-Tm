88af46f382b76182455dcf0cad34216d40af3a77
unknown <microsoft7779@gmail.com>
2023-02-07 21:37:23 +0500

    send ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Drawer from "@mui/material/Drawer";
+import { navbarStyle } from "../style/navbarStyle.mjs";
 
 function Navbar() {
   const [state, setState] = useState({
@@ -33,7 +34,7 @@ function Navbar() {
     </Box>
   );
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={navbarStyle}>
       <Container maxWidth="xl">
         <Box sx={{ display: { xs: "none", md: "flex" } }}>waleykimessalam</Box>
         <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
---
+++ src/style/navbarStyle.mjs
@@ -0,0 +1,3 @@
+export const navbarStyle = {
+  background: "#000",
+};
